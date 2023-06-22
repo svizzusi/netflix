6ceaa457b231173a74992ef98155532ed83e3926
Stephen Vizzusi <svizzusi13@gmail.com>
2023-06-21 23:57:47 -0700

    Added back in forms and deleted .DS_store from the root file on github. Also deleted the netlify website and redeploying under new name.

--- js/main.js
+++ js/main.js
@@ -23,25 +23,3 @@ for (i = 0; i < accordion.length; i++) {
   });
 }
 
-
-// let accordion = document.querySelectorAll(".accordion");
-// let x;
-// let activeAccordion = null;
-// for (x = 0; x < accordion.length; x++) {
-//     accordion[x].addEventListener("click", function() {
-//         if (activeAccordion && activeAccordion !== this) {
-//             let activePanel = activeAccordion.nextElementSibling;
-//             activePanel.style.display = "none";
-//             activeAccordion.classList.remove("active");
-//         }
-//         this.classlist.toggle("active");
-//         let panel = this.nextElementSibling;
-//         if (panel.style.display === "block") {
-//             panel.style.display = "none";
-//             activeAccordion = null;
-//         } else {
-//             panel.style.display = "block";
-//             activeAccordion = this;
-//         }
-//     })
-// }
